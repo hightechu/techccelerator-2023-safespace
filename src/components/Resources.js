9eb0d412b3585d529bae8e92b96648c00cb9d01b
High Tech U 02 <hightechu2023-02@uvic.ca>
2023-08-17 15:27:06 -0700

    added buttons to mental health resources, changed colors to the letters.

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -9,15 +9,21 @@ import React from 'react';
           Here are our favourite online resources and tools on mental health to help you learn about what might be going on for you. 
         </p>
         <h2>Mental health</h2>
-        <a href="https://headsupguys.org/" target="_blank">Heads Up Guys</a>
+       <div class="btn-group">
+        <a href="https://headsupguys.org/" class="buttonMh" target="_blank">Heads Up Guys</a>
         <br/>
-        <a href="https://youthspace.ca/youthspace-resources/" target="_blank">YouthSpace</a>
+        <a href="https://youthspace.ca/youthspace-resources/" class="buttonMh" target="_blank">YouthSpace</a>
         <br/>
-        <a href="https://www.anxietycanada.com/learn-about-anxiety/anxiety-in-youth/?_ga=2.193133357.1091829281.1692211004-1412770863.1692211004&_gl=1*jfwhxb*_ga*MTQxMjc3MDg2My4xNjkyMjExMDA0*_ga_Y4J3VSGKVS*MTY5MjIxMTAwMy4xLjEuMTY5MjIxMTAwNS4wLjAuMA.." target="_blank">Anxiety Canada</a>
+        <a href="https://www.anxietycanada.com/learn-about-anxiety/anxiety-in-youth/?_ga=2.193133357.1091829281.1692211004-1412770863.1692211004&_gl=1*jfwhxb*_ga*MTQxMjc3MDg2My4xNjkyMjExMDA0*_ga_Y4J3VSGKVS*MTY5MjIxMTAwMy4xLjEuMTY5MjIxMTAwNS4wLjAuMA.." class="buttonMh" target="_blank">Anxiety Canada</a>
+      </div>
         <h2>School and work</h2>
-        <a href="https://kidshelpphone.ca/get-info/going-to-school-50-student-wellness-resources/?msclkid=1b4356811624193eb1bf325c29455cac&utm_source=bing&utm_medium=cpc&utm_campaign=Kids%20Help%20Phone%20Paid_Gateway-Bing-NTL_2023-194_Search_Generic-NTL_En_CA&utm_term=educational%20stress&utm_content=Kids%20Help%20Phone%20Paid_Gateway-Bing-NTL_2023-194_Search_Generic-NTL_En_CA_School" target="_blank">Back to school - Kids Help Phone</a>
+       <div class="btn-group1"> 
+        <a href="https://kidshelpphone.ca/get-info/going-to-school-50-student-wellness-resources/?msclkid=1b4356811624193eb1bf325c29455cac&utm_source=bing&utm_medium=cpc&utm_campaign=Kids%20Help%20Phone%20Paid_Gateway-Bing-NTL_2023-194_Search_Generic-NTL_En_CA&utm_term=educational%20stress&utm_content=Kids%20Help%20Phone%20Paid_Gateway-Bing-NTL_2023-194_Search_Generic-NTL_En_CA_School" class="buttonMh1" target="_blank">Back to school - Kids Help Phone</a>
+       </div> 
         <h2>Relationships</h2>
-        <a href="https://kidshelpphone.ca/get-info/healthy-relationships-vs-unhealthy-relationships/?msclkid=e98b0ee1c38815834c5e6a3cdfdccbcc&utm_source=bing&utm_medium=cpc&utm_campaign=Kids%20Help%20Phone%20Paid_Gateway-Bing-NTL_2023-194_Search_Generic-NTL_En_CA&utm_term=relationship%20help&utm_content=Kids%20Help%20Phone%20Paid_Gateway-Bing-NTL_2023-194_Search_Generic-NTL_En_CA_Relationships" target="_blank">Healthy Relationships - Kids Help Phone</a>
+        <div class="btn-group2">
+        <a href="https://kidshelpphone.ca/get-info/healthy-relationships-vs-unhealthy-relationships/?msclkid=e98b0ee1c38815834c5e6a3cdfdccbcc&utm_source=bing&utm_medium=cpc&utm_campaign=Kids%20Help%20Phone%20Paid_Gateway-Bing-NTL_2023-194_Search_Generic-NTL_En_CA&utm_term=relationship%20help&utm_content=Kids%20Help%20Phone%20Paid_Gateway-Bing-NTL_2023-194_Search_Generic-NTL_En_CA_Relationships" class="buttonMh2" target="_blank">Healthy Relationships - Kids Help Phone</a>
+        </div>
         <h2>Substance abuse</h2>
         <a href="https://ccsa.ca/youth-and-emerging-adults" target="_blank">Canadian Centre on Substance Use and Addiction</a>
         <br/>
